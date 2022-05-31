d8d5ce2bad32f0d51c8e52faef6507f2febc963f
wmb6598 <wmb6598@gmail.com>
2022-05-31 17:44:06 +0100

    cm

--- Regular_expressions/Regular_expressions.py
+++ Regular_expressions/Regular_expressions.py
@@ -2,3 +2,8 @@ import re
 
 
 import re # Regular expressions arent in the default python library
+hand = open('Course_assets\intro.txt')
+for line in hand :
+    line = line.rstrip()
+    if re.search('From:', line) :
+        print(line)
